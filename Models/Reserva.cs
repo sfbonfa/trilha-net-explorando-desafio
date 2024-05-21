196991cb7123456efd492bfbeb710e8582c7df01
Suzana Bonfá <suzanafernandess@gmail.com>
2024-05-21 16:55:31 -0300

    Desafio concluido!

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,14 +17,17 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+                        
+            if (hospedes.Count <= Suite.Capacidade)
             {
+                
                 Hospedes = hospedes;
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                throw new Exception("A quantidade de hóspedes não pode exceder a capacidade da suíte");
             }
         }
 
@@ -37,7 +40,7 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes != null ? Hospedes.Count : 0;
         }
 
         public decimal CalcularValorDiaria()
@@ -45,16 +48,16 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valorDiaria = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >=10)
             {
-                valor = 0;
+                valorDiaria *= 0.9m;
             }
 
-            return valor;
+            return valorDiaria;
         }
     }
 }
